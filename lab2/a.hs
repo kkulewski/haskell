bfc21f75f98ad438c2e87019fd6eca576ed0b6da
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2019-04-06 10:15:51 +0200

    Add ex 17

--- lab2/a.hs
+++ lab2/a.hs
@@ -82,3 +82,86 @@ andl xs = foldl (and1) True xs
 
 andr [] = False
 andr xs = foldr (and1) True xs
+
+-- d)
+
+-- e)
+
+-- f)
+
+-- g)
+
+-- h)
+filter1 pred [] = []
+filter1 pred (x:xs) = foldl (\x xs -> if pred x then x else xs) [] xs
+
+-- i)
+
+
+
+-- ZAD 14
+
+
+-- ZAD 15
+
+
+-- ZAD 16
+
+
+-- ZAD 17
+square x = x * x
+f x = x
+g x = x
+
+-- a) (+) :: Num a => a -> a -> a
+-- b) (+ 37) :: Num a => a -> a
+-- c) append1 :: [a] -> [a] -> [a]
+-- d) append1 [1,2] :: Num a => [a] -> [a]
+-- e) map :: (a -> b) -> [a] -> [b]
+-- f) (map square [1,2,3,4,5]) :: Num b => [b]
+-- g) ?
+-- h) map length [['a']] :: [Int]
+-- i) foldl :: Foldable t => (b -> a -> b) -> b -> t a -> b
+-- j) foldl (++) :: Foldable t => [a] -> t [a] -> [a]
+-- k) foldl (++) [] :: Foldable t => t [a] -> [a]
+
+
+
+
+
+
+
+-- l) e = f 7
+-- l) { f :: (Int -> a)} |- e :: a
+
+
+-- m) e = (\f -> f 7)
+-- m) { } |- e :: (Int -> a) -> a
+
+-- n) e = + (f x) (g x)
+-- n) { f :: (a -> Int), g :: (a -> Int), x :: a } |- e :: Int
+
+-- o) e = f 7 (g 'x')
+-- o) { f :: Int -> b -> c, g :: Char -> b} |- e :: c
+
+-- p) e = \f -> f (g x)
+-- p) { g :: (c -> a), x :: c } |- e :: (a -> b) -> b
+
+-- q) e = (\f -> f (g x)) square
+-- q) { g :: (c -> a), x :: c} |- e :: Int
+
+
+
+
+
+
+
+-- n) + (f x) (g x) 
+-- n) (a -> b) -> (a -> b) -> b
+
+-- l) f :: Num a => a -> a
+-- m) \f -> f 7) :: Num t1 => (t1 -> t2) -> t2
+-- n) (+ (f 1) (g 1)) :: (Num a, Num t, Num (t -> a)) => a -> a
+-- o) (f 7 (g 'x')) :: Num (Char -> t) => t
+-- p) (\f -> f (g 1)) :: Num t1 => (t1 -> t2) -> t2
+-- q) ((\f -> f (g 1)) square) :: Num t => t
